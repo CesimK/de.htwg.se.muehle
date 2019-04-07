18a17bb9950ea4abeab9a0858ffbaa9ec82f160b
ChristopherGogl <christopher.gogl@web.de>
2019-04-07 15:28:37 +0200

    update main class

--- src/main/scala/de/htwg/se/muehle/Muehle.scala
+++ src/main/scala/de/htwg/se/muehle/Muehle.scala
@@ -2,9 +2,9 @@ package de.htwg.se.muehle.model
 
 object Muehle {
   def main(args: Array[String]): Unit = {
-    val player1 = Player("Cesim Keskin")
+    val player1 = Player("Cesim Keskin", "White")
     println("Hello, " + player1.name)
-    val player2 = Player("Christopher Gogl")
+    val player2 = Player("Christopher Gogl", "Black")
     println("Hello, " + player2.name)
   }
 }

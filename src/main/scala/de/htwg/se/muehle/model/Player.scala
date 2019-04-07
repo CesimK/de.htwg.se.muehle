09f7eaabf307f4149a4db8c64fbef772a51f33d6
ChristopherGogl <christopher.gogl@web.de>
2019-04-07 15:24:51 +0200

    refactor player model

--- src/main/scala/de/htwg/se/muehle/model/Player.scala
+++ src/main/scala/de/htwg/se/muehle/model/Player.scala
@@ -1,5 +1,9 @@
 package de.htwg.se.muehle.model;
 
-case class Player(name: String) {
-  override def toString:String = name
+case class Player(name: String, team:String) {
+  val stones:Int = 9
+  val jump:Boolean = false
+  val placed:Int = 0
+
+  override def toString:String = "%s (%s)".format(name, team)
 }
--- src/test/scala/de/htwg/se/muehle/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/muehle/model/PlayerSpec.scala
@@ -5,18 +5,21 @@ import org.scalatest._
 @Ignore
 class PlayerSpec extends WordSpec with Matchers {
   "A Player" when { "new" should {
-    val player = Player("Your Name")
+    val player = Player("Your Name", "White")
     "have a name"  in {
       player.name should be("Your Name")
     }
     "have a nice String representation" in {
-      player.toString should be("Your Name")
+      player.toString should be("Your Name (White)")
     }
     "Also the player should have the start amount of 9 stone" in {
       player.stones shuld be("9")
     }
     "With all these stones the player is not be able to jump with his stones" in {
       player.jump should be(false)
+    }
+    "When the game has not started yet, ther also shoould't be placed a stone." in {
+      player.placed should be (0)
     }
   }}
 }

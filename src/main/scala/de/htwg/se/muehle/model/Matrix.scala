4f881a805c339f4e8fc50df96db6d12f94b92332
ChristopherGogl <christopher.gogl@web.de>
2019-04-12 00:07:18 +0200

    Add missing =, remove override where no override is

--- src/main/scala/de/htwg/se/muehle/model/Matrix.scala
+++ src/main/scala/de/htwg/se/muehle/model/Matrix.scala
@@ -3,5 +3,5 @@ package de.htwg.se.muehle.model
 case class Matrix[T](grid:Vector[Vector[T]]) {
   val rows:Int = grid.length()
   val cols:Int = grid.head.length()
-  override def size:Int rows * cols
+  def size:Int = rows * cols
 }

4744be872924a079f3f2f3f00cdd42edef6e8ac7
CesimK <Cesim.Keskin@aol.com>
2019-04-12 09:37:16 +0200

    Refactored

--- src/main/scala/de/htwg/se/muehle/model/Player.scala
+++ src/main/scala/de/htwg/se/muehle/model/Player.scala
@@ -1,9 +1,9 @@
 package de.htwg.se.muehle.model;
 
-case class Player(name: String, team:String) {
+case class Player(name: String, color:String) {
   val stones:Int = 9
   val jump:Boolean = false
   val placed:Int = 0
 
-  override def toString:String = "%s (%s)".format(name, team)
+  override def toString:String = "%s (%s)".format(name, color)
 }

d954f1c479cf19b9f499d56e032ba44d7854116f
CesimK <Cesim.Keskin@aol.com>
2019-04-12 11:10:20 +0200

    started working on Grid

---
+++ src/main/scala/de/htwg/se/muehle/model/Grid.scala
@@ -0,0 +1,4 @@
+package de.htwg.se.muehle.model
+//TODO: Exchange Matrix with Edges and Vertexes; impl utils with Edges and mills;
+class Grid {
+}
---
+++ src/test/scala/de/htwg/se/muehle/model/GridSpec.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.muehle.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GridSpec extends WordSpec with Matchers {
+  "A Field" when( "new" should{
+
+  })
+}

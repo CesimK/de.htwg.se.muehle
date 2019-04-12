51e1a594265846f289573488df066ef85d650d23
CesimK <Cesim.Keskin@aol.com>
2019-04-12 09:41:45 +0200

    fix in Matrix.scala

--- src/test/scala/de/htwg/se/muehle/model/MatrixSpec.scala
+++ src/test/scala/de/htwg/se/muehle/model/MatrixSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest._
 class MatrixSpec extends WordSpec with Matchers {
   "A Matrix" when { "new" should {
     val vector = Vector[Cell](Cell())
-    val matrix = Matrix[Cell](Vector[Vector[T]](vector))
+    val matrix = Matrix[Cell](Vector[Vector[Cell]](vector))
     "produce a grid size with 1 times 1" in {
       matrix.size should be (1)
       matrix.rows should be (1)

8fd18584b396be9a3ee5c5d0f361b8381cb1fac4
ChristopherGogl <christopher.gogl@web.de>
2019-04-14 16:14:20 +0200

    Set Output text for each command add general help text

--- src/main/scala/de/htwg/se/muehle/Muehle.scala
+++ src/main/scala/de/htwg/se/muehle/Muehle.scala
@@ -1,10 +1,21 @@
 package de.htwg.se.muehle.model
 
+import scala.io.StdIn.readLine
+
 object Muehle {
+  val tui = new Tui
+  val grid = new Grid()
+
   def main(args: Array[String]): Unit = {
+    var input: String = ""
     val player1 = Player("Cesim Keskin", "White")
     println("Hello, " + player1.name)
     val player2 = Player("Christopher Gogl", "Black")
     println("Hello, " + player2.name)
+
+    do {
+      input = readLine()
+      tui.process_cmd(input, grid)
+    }
   }
 }
--- src/main/scala/de/htwg/se/muehle/aview/Tui.scala
+++ src/main/scala/de/htwg/se/muehle/aview/Tui.scala
@@ -3,18 +3,41 @@ package de.htwg.se.muehle.model;
 class Tui {
   def process_cmd(flag:String, grid:Matrix[T]):Matrix[T] = {
    flag match {
-     case 'q' | 'quit'        => println("This function is without function yet" )
-     case 'n' | 'new'         => println("This function is without function yet" )
-     case 'm' | 'move'        => println("This function is without function yet" )
-     case 'u' | 'undo'        => println("This function is without function yet" )
-     case 'r' | 'redo'        => println("This function is without function yet" )
-     case 's' | 'save'        => println("This function is without function yet" )
-     case 'l' | 'load'        => println("This function is without function yet" )
-     case 'sur' | 'surrender' => println("This function is without function yet" )
-     case 'h' | '?' | 'help'  => println("This function is without function yet" )
-     case _                   => println("This function is without function yet" )
-
+     case 'q' | 'quit'        => println("Closing the game. All unsaved changes will be lost.")
+     case 'n' | 'new'         => println("Starting a new game")
+     case 'm' | 'move'        => println("Move a Stone to a new position.")
+     case 'u' | 'undo'        => println("Undo the last turn")
+     case 'r' | 'redo'        => println("Redo the last turn")
+     case 's' | 'save'        => println("Save the game")
+     case 'l' | 'load'        => println("Load the game")
+     case 'sur' | 'surrender' => println("Give up")
+     case 'h' | '?' | 'help'  => println(this.help_text())
+     case _                   => println("This command does not exists.\nPlease see the help which commands are allowed.")
    }
+  }
 
+  def help_text():String = {
+      val text:String = "The following commands can be used to controll the game.\n"+
+        "To get more information about a command use:\n" +
+        "\t<h|help> <command>\n\n" +
+        "q | quit:\n" +
+        "\tClose the game. The actual progress won't be saved. All changes are lost.\n\n" +
+        "n | new:\n"+
+        "\tStart a new game. The current progress get lost and won't be saved.\n\n" +
+        "m | move:\n"+
+        "\tMove a Stone from one field to another.\n\n" +
+        "u | undo:\n"+
+        "\tUndo the last turn.\n\n" +
+        "r | redo:\n"+
+        "\tRedo the last undo turn.\n\n" +
+        "s | save:\n"+
+        "\tSave the current state of the game.\n\n" +
+        "l | load:\n"+
+        "\tLoad a previous saved game.\n\n" +
+        "sur | surrender:\n"+
+        "\tGive up the game. The other player will be declared as winner.\n\n" +
+        "h | ? | help:\n"+
+        "\tShows this help text."
+      retrun text
   }
 }

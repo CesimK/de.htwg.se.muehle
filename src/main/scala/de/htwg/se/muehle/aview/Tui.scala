ed2f3823f0c72c21d6ea54f38c65a510fbd8e909
CesimK <Cesim.Keskin@aol.com>
2019-04-26 12:07:08 +0200

    There is only one way to do it, the right way

--- src/main/scala/de/htwg/se/muehle/aview/Tui.scala
+++ src/main/scala/de/htwg/se/muehle/aview/Tui.scala
@@ -5,21 +5,21 @@ import model.Field
 class Tui () {
   def process_cmd(flag:String, grid:Field):String = {
    flag match {
-     case "q" | "quit"        => return "Closing the game. All unsaved changes will be lost."
-     case "n" | "new"         => return "Starting a new game"
-     case "m" | "move"        => return "Move a Stone to a new position."
-     case "u" | "undo"        => return "Undo the last turn"
-     case "r" | "redo"        => return "Redo the last turn"
-     case "s" | "save"        => return "Save the game"
-     case "l" | "load"        => return "Load the game"
-     case "sur" | "surrender" => return "Give up"
-     case "h" | "?" | "help"  => return this.help_text()
-     case _                   => return "This command does not exists.\nPlease see the help which commands are allowed."
+     case "q" | "quit"        => "Closing the game. All unsaved changes will be lost."
+     case "n" | "new"         => "Starting a new game"
+     case "m" | "move"        => "Move a Stone to a new position."
+     case "u" | "undo"        => "Undo the last turn"
+     case "r" | "redo"        => "Redo the last turn"
+     case "s" | "save"        => "Save the game"
+     case "l" | "load"        => "Load the game"
+     case "sur" | "surrender" => "Give up"
+     case "h" | "?" | "help"  => this.help_text()
+     case _                   => "This command does not exists.\nPlease see the help which commands are allowed."
    }
   }
 
-  def help_text():String = {
-      val text:String = "The following commands can be used to controll the game.\n"+
+  def help_text():String =
+      "The following commands can be used to controll the game.\n"+
         "To get more information about a command use:\n" +
         "\t<h|help> <command>\n\n" +
         "q | quit:\n" +
@@ -40,6 +40,4 @@ class Tui () {
         "\tGive up the game. The other player will be declared as winner.\n\n" +
         "h | ? | help:\n"+
         "\tShows this help text."
-      return text
-  }
 }

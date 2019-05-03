df119fb730b0910d8d11d2a8b0cb87acb63134d9
CesimK <Cesim.Keskin@aol.com>
2019-05-03 18:00:15 +0200

    Forgot to commit this piece of Art

--- src/main/scala/de/htwg/se/muehle/controller/Controller.scala
+++ src/main/scala/de/htwg/se/muehle/controller/Controller.scala
@@ -1,11 +1,11 @@
 package de.htwg.se.muehle.controller
 
-import de.htwg.se.muehle.model.Field
+import de.htwg.se.muehle.model.Grid
 import de.htwg.se.muehle.util.Observable
 
-class Controller(var grid:Field) extends Observable{
+class Controller(var grid:Grid) extends Observable{
   def createEmptyGrid():Unit = {
-    grid = Field(init = true)
+    grid = Grid(init = true)
     notifyObservers
   }
 

9b80f63510dd89128c4c0be1a1dddf26d7910642
Chris <christopher.gogl@web.de>
2019-05-03 17:19:30 +0200

    update player and main class to use

--- src/main/scala/de/htwg/se/muehle/Muehle.scala
+++ src/main/scala/de/htwg/se/muehle/Muehle.scala
@@ -1,21 +1,21 @@
-package de.htwg.se.muehle.model
+package de.htwg.se.muehle
 
 import de.htwg.se.muehle.model.{Field, Player}
 import de.htwg.se.muehle.controller.Controller
 import de.htwg.se.muehle.aview.Tui
 
 object Muehle {
-  val field = new Field()
-  val controller = new Controller(field)
-  val tui = new Tui(controller)
 
   def main(args: Array[String]): Unit = {
     var input: String = ""
-    val player1 = Player("Cesim Keskin", "White")
+    val player1 = Player("Cesim Keskin", 'W')
     println("Hello, " + player1.name)
-    val player2 = Player("Christopher Gogl", "Black")
+    val player2 = Player("Christopher Gogl", 'B')
     println("Hello, " + player2.name)
 
+    val field = Field()
+    val controller = new Controller(field, player1, player2)
+    val tui = new Tui(controller)
     do {
       input = scala.io.StdIn.readLine()
       tui.process_cmd(input)

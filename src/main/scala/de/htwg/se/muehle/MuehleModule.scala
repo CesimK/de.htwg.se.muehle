13124e1ba8f58d3a7c03141549ff62fe9de874d6
CesimK <Cesim.Keskin@aol.com>
2019-07-04 18:22:53 +0200

    test some more shit

--- src/main/scala/de/htwg/se/muehle/MuehleModule.scala
+++ src/main/scala/de/htwg/se/muehle/MuehleModule.scala
@@ -3,7 +3,7 @@ package de.htwg.se.muehle
 import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
 import de.htwg.se.muehle.controller.controllerComponent.{IController, controllerBaseImpl}
-import de.htwg.se.muehle.model.fileIOImpl.{FileIOInterface, jsonImpl}
+import de.htwg.se.muehle.model.fileIOImpl.{FileIOInterface, jsonImpl, xmlImpl}
 import de.htwg.se.muehle.model.gridComponent.{IGrid, gridBaseImpl}
 
 
@@ -12,6 +12,6 @@ class MuehleModule extends AbstractModule with ScalaModule {
   def configure() = {
     bind[IController].to[controllerBaseImpl.Controller]
     bind[IGrid].to[gridBaseImpl.Grid]
-    bind[FileIOInterface].to[jsonImpl.FileIO]
+    bind[FileIOInterface].to[xmlImpl.FileIO]
   }
 }
